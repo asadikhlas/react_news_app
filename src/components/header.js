70a84f9bf7eb4bda73761a898ccc557cb2cccff2
Asad Ikhlas <33983874+asadikhlas@users.noreply.github.com>
2019-01-01 23:27:28 +0500

    adding state

--- src/components/header.js
+++ src/components/header.js
@@ -3,8 +3,16 @@ import '../css/styles.css'
  
  
 class Header extends Component{ 
+
+    state = {
+        title:'The keywords are:',
+        keywords:''
+    }
     inputchangeHandler(event){
-        console.log(event.target.value)
+        // console.log(event.target.value)
+        this.setState({
+            keywords: event.target.value
+        });
     }
     render (){ 
     return (
@@ -14,9 +22,11 @@ class Header extends Component{
          type="text"
          onChange={(e) => this.inputchangeHandler(e)}
          />
+         <div>{this.state.title}</div>
+         <div>{this.state.keywords}</div>
     </header>
         )
     }
-}
+} 
 
 export default Header;  
